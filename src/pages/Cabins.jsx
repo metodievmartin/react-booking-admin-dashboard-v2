b1722048ad5e1e40a017d117ac9c813a7639c269
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-22 22:24:53 +0200

    Clean up imports

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,6 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { useEffect } from 'react';
-import { getCabins } from '../services/apiCabins.js';
-import CabinTable from '../features/cabins/CabinTable.jsx';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
   return (
