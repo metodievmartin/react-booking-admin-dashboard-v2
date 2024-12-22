7e7a60496258d5ecfb306eabf13c5ee804672c78
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-22 19:41:42 +0200

    Add a query client provider and a client

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyles from './styles/GlobalStyles.js';
+import AppLayout from './ui/AppLayout.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Cabins from './pages/Cabins.jsx';
 import Users from './pages/Users.jsx';
 import Account from './pages/Account.jsx';
 import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
-import AppLayout from './ui/AppLayout.jsx';
 import Bookings from './pages/Bookings.jsx';
 import Settings from './pages/Settings.jsx';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +42,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
