f2d8462746023852755c1b7d6ee92e80fa2049e1
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-22 11:45:01 +0200

    Set up the route structure with placeholder page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import GlobalStyles from './styles/GlobalStyles.js';
+import Dashboard from './pages/Dashboard.jsx';
+import Cabins from './pages/Cabins.jsx';
+import Users from './pages/Users.jsx';
+import Account from './pages/Account.jsx';
+import Login from './pages/Login.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>Hello World!!</div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cabins" element={<Cabins />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Users />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
