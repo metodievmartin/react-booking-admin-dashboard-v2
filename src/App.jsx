f0ddd21526a23f453da493e8222ac6d69759f969
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-22 13:05:44 +0200

    Add Users and Settings route pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Account from './pages/Account.jsx';
 import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 import AppLayout from './ui/AppLayout.jsx';
+import Bookings from './pages/Bookings.jsx';
+import Settings from './pages/Settings.jsx';
 
 function App() {
   return (
@@ -19,8 +21,9 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/cabins" element={<Cabins />} />
+            <Route path="/bookings" element={<Bookings />} />
             <Route path="/users" element={<Users />} />
-            <Route path="/settings" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
             <Route path="/account" element={<Account />} />
           </Route>
           <Route path="/login" element={<Login />} />
