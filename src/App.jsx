874c4661a76be920e11808635955eefdf6042fa6
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-27 21:01:48 +0200

    Implement a Booking Details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 import GlobalStyles from './styles/GlobalStyles.js';
 import AppLayout from './ui/AppLayout.jsx';
@@ -12,7 +13,7 @@ import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 import Bookings from './pages/Bookings.jsx';
 import Settings from './pages/Settings.jsx';
-import { Toaster } from 'react-hot-toast';
+import Booking from './pages/Booking';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -36,6 +37,7 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/cabins" element={<Cabins />} />
             <Route path="/bookings" element={<Bookings />} />
+            <Route path="/bookings/:bookingId" element={<Booking />} />
             <Route path="/users" element={<Users />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/account" element={<Account />} />
--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -5,6 +5,9 @@ import Tag from '../../ui/Tag';
 import Table from '../../ui/Table';
 
 import { formatCurrency, formatDistanceFromNow } from '../../utils/helpers';
+import Menus from '../../ui/Menus';
+import { HiEye } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -47,6 +50,7 @@ const BookingRow = ({
     cabins: { name: cabinName },
   },
 }) => {
+  const navigate = useNavigate();
   const statusToTagName = {
     unconfirmed: 'blue',
     'checked-in': 'green',
@@ -78,6 +82,18 @@ const BookingRow = ({
       <Tag type={statusToTagName[status]}>{status.replace('-', ' ')}</Tag>
 
       <Amount>{formatCurrency(total_price)}</Amount>
+
+      <Menus.Menu>
+        <Menus.Toggle id={bookingId} />
+        <Menus.List id={bookingId}>
+          <Menus.Button
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+            icon={<HiEye />}
+          >
+            See details
+          </Menus.Button>
+        </Menus.List>
+      </Menus.Menu>
     </Table.Row>
   );
 };
