736b1d25036a174542e5f490b62a5109df574775
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-27 18:21:24 +0200

    Add pre-fetching of the previous and the next pages on the bookings table for seamless pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
 
 import { getBookings } from '../../services/apiBookings';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export const useBookings = () => {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // Filtering
@@ -25,6 +27,7 @@ export const useBookings = () => {
     ? Number(searchParams.get('page'))
     : 1;
 
+  // Query
   const { isLoading, data, error } = useQuery({
     // adding 'filter' and 'sortBy' here will cause React Query to re-fetch the data if any of them changes
     queryKey: ['bookings', filter, sortBy, currentPage],
@@ -34,6 +37,26 @@ export const useBookings = () => {
   if (data) {
     bookings = data.bookings;
     totalResults = data.count;
+  }
+
+  // Pre-fetching
+  // Pre-fetch the next and previous pages of bookings for a seamless pagination experience.
+  const pageCount = Math.ceil(totalResults / PAGE_SIZE);
+
+  // Ensure the next page exists to prevent unnecessary requests or errors.
+  if (currentPage < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, currentPage + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: currentPage + 1 }),
+    });
+  }
+
+  // Ensure the previous page exists to prevent unnecessary requests or errors.
+  if (currentPage > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, currentPage - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: currentPage - 1 }),
+    });
   }
 
   return { isLoading, bookings, totalResults, error };
