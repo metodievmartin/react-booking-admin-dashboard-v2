d0904af5c9413bbbd406c092c51f6213c4a2980e
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-29 12:19:42 +0200

    Add the user to cache after logging in to prevent re-fetching again right after that

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 import { login as loginAPI } from '../../services/apiAuth';
 
 export const useLogin = () => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
     onSuccess: (user) => {
       console.log(user);
+      queryClient.setQueriesData(['user'], user);
       navigate('/dashboard');
     },
     onError: (error) => {
