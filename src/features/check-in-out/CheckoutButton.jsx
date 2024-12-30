ebfb868ee6d026c3180603eda3308f12ee14e51b
Martin Metodiev <martin.metodiew@gmail.com>
2024-12-30 17:37:16 +0200

    Implement today's activity table - pulling all guests leaving or arriving today

---
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -0,0 +1,19 @@
+import Button from '../../ui/Button';
+import { useCheckout } from './useCheckout';
+
+function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
+  return (
+    <Button
+      onClick={() => checkout(bookingId)}
+      variation="primary"
+      size="small"
+      disabled={isCheckingOut}
+    >
+      Check out
+    </Button>
+  );
+}
+
+export default CheckoutButton;
---
+++ src/features/check-in-out/TodayItem.jsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import Tag from '../../ui/Tag';
+import { Flag } from '../../ui/Flag';
+import Button from '../../ui/Button';
+import CheckoutButton from './CheckoutButton';
+
+const StyledTodayItem = styled.li`
+  display: grid;
+  grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
+  gap: 1.2rem;
+  align-items: center;
+
+  font-size: 1.4rem;
+  padding: 0.8rem 0;
+  border-bottom: 1px solid var(--color-grey-100);
+
+  &:first-child {
+    border-top: 1px solid var(--color-grey-100);
+  }
+`;
+
+const Guest = styled.div`
+  font-weight: 500;
+`;
+
+const TodayItem = ({ activity }) => {
+  const { id, status, guests, num_of_nights } = activity;
+  console.log(guests);
+  const isArriving = status === 'unconfirmed';
+  const isLeaving = status === 'checked-in';
+
+  return (
+    <StyledTodayItem>
+      {isArriving && <Tag type="green">Arriving</Tag>}
+      {isLeaving && <Tag type="blue">Departing</Tag>}
+
+      <Flag src={guests.country_flag} alt={`Flag of ${guests.nationality}`} />
+      <Guest>{guests.full_name}</Guest>
+      <div>{num_of_nights} nights</div>
+
+      {isArriving && (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check In
+        </Button>
+      )}
+
+      {isLeaving && <CheckoutButton bookingId={id} />}
+    </StyledTodayItem>
+  );
+};
+
+export default TodayItem;
